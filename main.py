dd63625102594eed07095cdee1a77541286659de
clwon1320 <hby1320@gmail.com>
2021-08-07 14:41:56 +0900

    fix module & main.py fix

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from torchvision.datasets import VOCDetection, CocoDetection, ImageFolder
 from torchvision.transforms.functional import to_tensor, to_pil_image
 from torchvision.transforms import transforms
 from torch.utils.data import Dataset, DataLoader
-from PIL import Image, ImageDraw, ImageFont  # iamgeprossesing
-import albumentations as A  # Data agumentation
+from PIL import Image, ImageDraw, ImageFont  # image processing
+import albumentations as a  # Data augmentation
 from albumentations.pytorch import ToTensorV2
 import matplotlib.pyplot as plt
 import os
@@ -25,43 +25,21 @@ batch = 1
 local_rank = -1
 path2data = './Data/voc'
 backbone = 'efficientnet-b0'
+# train_set = []
+
+# for i in range(2):
+#     yr = ['2007','2012'] # 2007 : 2501
+#     Set = ['train', 'tes', 'val']
+#     print(f'{yr[i]}')
+#     data = dataload_voc(path2data, year = yr[i], image_set = 'train', download = False)
+#     train_set += data
+#     print(f'train_set:{len(train_set)}')
 
 
 
 
-
-
-# train_12_ds = dataload_voc(path2data, year = '2012', image_set = 'train', download = False, transform = transform_train)
-# train_07_ds = dataload_voc(path2data, year = '2007', image_set = 'train', download = False, transform = transform_train)
-train_12_ds = dataload_voc(path2data, year = '2012', image_set = 'train', download = False, transform = transform_train)
-train_07_ds = dataload_voc(path2data, year = '2007', image_set = 'train', download = False, transform = transform_train)
-trainset = DataLoader(train_12_ds+train_07_ds, 1, shuffle = True) # <class 'torch.utils.data.dataloader.DataLoader'>
-transform_train = transforms.Compose([transforms.Resize(224), transforms.ToTensor(),
-                                      transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),])
-
-
-#
-# def custom_imshow(img):
-#     img = img.numpy()
-#     plt.imshow(np.transpose(img, (1, 2, 0)))
-#     plt.show()
-
-
-
-
-# model.eval()
-# for i in range(len(train_07_ds)):
-#     img = trainset[i]
-#     with torch.no_grad():
-#         outputs = model(img)
-#
-# # transforms 정의
-# IMAGE_SIZE = 512
-# scale = 1.0
-#
-# #
-# # # transforms 적용하기
-# # trainset.transforms = train_transforms
-
-
-#
+# train_12_ds = dataload_voc(path2data, year = '2012', image_set = 'train', download = False)
+# train_07_ds = dataload_voc(path2data, year = '2007', image_set = 'train', download = False)
+# train_set = DataLoader(train_12_ds+train_07_ds, 1, shuffle = True) # <class 'torch.utils.data.dataloader.DataLoader'>
+# transform_train = transforms.Compose([transforms.Resize(224), transforms.ToTensor(),
+#                                       transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),])
