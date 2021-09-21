d0ed58a8b46944f364507972dd8d3774e9759594
clwon1320 <hby1320@gmail.com>
2021-09-22 00:12:49 +0900

    proposed method fix

--- model/od/proposed.py
+++ model/od/proposed.py
@@ -5,10 +5,22 @@ from model.modules.modules import PointWiseConv, DepthWiseConv2d
 from model.backbone.resnet50 import ResNet50
 from utill.utills import model_info
 from typing import List
+import numpy as np
 
 
 class FRFCOS(nn.Module):
+    """
+    Total params: 38,963,484
+    Trainable params: 35,920,348
+    Non-trainable params: 3,043,136
+    Total mult-adds (G): 149.93
+    Input size (MB): 3.15
+    Forward/backward pass size (MB): 1198.24
+    Params size (MB): 155.85
+    Estimated Total Size (MB): 1357.24
+    """
     def __init__(self, feature_map:List[int],
+                 feature_lv:List[int],
                  num_classes:int,
                  feature: int,
                  bn_freeze:bool = True):
@@ -19,15 +31,17 @@ class FRFCOS(nn.Module):
                                ,bias = False)
         self.bn = nn.BatchNorm2d(feature_map[0])
         self.act = nn.SiLU(True)
-        self.icsp_blcok1 = ICSPBlock(512, 512)
-        self.icsp_blcok2 = ICSPBlock(512, 256)
-        self.icsp_blcok3 = ICSPBlock(256, 128)
-        self.Up_sample1 = nn.Upsample(scale_factor = 2)
-        self.Up_sample2 = nn.Upsample(scale_factor = 2)
-        self.refine = Refine_Module()
-        self.head_s = RfFcosHead(128, feature, num_classes)
-        self.head_m = RfFcosHead(256, feature, num_classes)
-        self.head_l = RfFcosHead(512, feature, num_classes)
+        self.fpn = ICSPFPN(feature_map, feature_lv)
+        self.refine = Refine_Module(feature_lv, 512)
+        self.tf1 = PointWiseConv(feature_lv[2], feature)
+        self.tf2 = PointWiseConv(feature_lv[1], feature)
+        self.tf3 = PointWiseConv(feature_lv[0], feature)
+        self.cls_net = ClassificationSub(feature, num_classes, 0.01)
+        self.reg_net = RegressionSub(feature)
+        self.scale_exp = nn.ModuleList([ScaleExp(1.0) for _ in range(5)])
+        # self.head_s = RfFcosHead(128, feature, num_classes)
+        # self.head_m = RfFcosHead(256, feature, num_classes)
+        # self.head_l = RfFcosHead(512, feature, num_classes)
 
         def freeze_bn(module):
             if isinstance(module,nn.BatchNorm2d):
@@ -49,26 +63,49 @@ class FRFCOS(nn.Module):
         reg = []
         x1, x2, x3 = self.backbone(x)  # 512 * 64, 1024 * 32, 2048  * 16
         x4 = self.bn(self.act(self.conv1(x3)))  # 512 * 8
-        p5 = self.icsp_blcok1(x4)
-        p6 = self.Up_sample1(p5)
-        p6 = self.icsp_blcok2(p6)
-        p7 = self.Up_sample2(p6)
-        p7 = self.icsp_blcok3(p7)
-        r3,r2,r1 = self.refine([p5,p6,p7])
-        r1_cls,r1_cnt,r1_reg = self.head_s(r1)
-        r2_cls,r2_cnt,r2_reg = self.head_m(r2)
-        r3_cls,r3_cnt,r3_reg = self.head_l(r3)
-        cls.append(r1_cls)
-        cls.append(r2_cls)
-        cls.append(r3_cls)
-        cnt.append(r1_cnt)
-        cnt.append(r2_cnt)
-        cnt.append(r3_cnt)
-        reg.append(r1_reg)
-        reg.append(r2_reg)
-        reg.append(r3_reg)
+        x = self.fpn([x1,x2,x3,x4])
+        x1, x2, x3 = self.refine(x)
+        x1 = self.tf1(x1)
+        x2 = self.tf2(x2)
+        x3 = self.tf3(x3)
+        x = [x1,x2,x3]
+        for i, feature in enumerate(x):
+            cls_logit = self.cls_net(feature)
+            center_logit, reg_logit = self.reg_net(feature)
+            cls.append(cls_logit)
+            cnt.append(center_logit)
+            reg.append(self.scale_exp[i](reg_logit))
 
         return cls, cnt, reg
+
+
+class ICSPFPN(nn.Module):
+    def __init__(self, feature_map: List[int], feature_lv: List[int]):
+        super(ICSPFPN, self).__init__()
+        self.tf1 = PointWiseConv(feature_map[2],feature_lv[2])
+        self.tf2 = PointWiseConv(feature_map[1],feature_lv[1])
+        self.tf3 = PointWiseConv(feature_map[0],feature_lv[0])
+        self.icsp_blcok1 = ICSPBlock(feature_lv[2], feature_lv[2])
+        self.icsp_blcok2 = ICSPBlock(feature_lv[2], feature_lv[1])
+        self.icsp_blcok3 = ICSPBlock(feature_lv[1], feature_lv[0])
+        self.Up_sample1 = nn.Upsample(scale_factor = 2)
+        self.Up_sample2 = nn.Upsample(scale_factor = 2)
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x1, x2, x3, x4 = x
+        p5 = self.icsp_blcok1(x4)
+        x3 = self.tf1(x3)
+        p5 = torch.add(p5, x3)
+        p6 = self.Up_sample1(p5)
+        p6 = self.icsp_blcok2(p6)
+        x2 = self.tf2(x2)
+        p6 = torch.add(p6, x2)
+        p7 = self.Up_sample2(p6)
+        p7 = self.icsp_blcok3(p7)
+        x1 = self.tf3(x1)
+        p7 = torch.add(p7, x1)
+        return p5, p6, p7
 
 
 class ICSPBlock(nn.Module):
@@ -81,8 +118,6 @@ class ICSPBlock(nn.Module):
         self.pw_conv3 = PointWiseConv(in_channel = in_ch, out_channel = in_ch)
         self.pw_conv4 = PointWiseConv(in_channel = in_ch * 2, out_channel = out_ch)
         self.pw_conv5 = PointWiseConv(in_channel = in_ch, out_channel = in_ch)
-        # self.conv1 = nn.Conv2d(in_channels = in_ch, out_channels = in_ch, kernel_size = 3, padding = 1
-        #                         , bias = False)
         self.se_block = SE_Block(in_ch, alpha = alpha)
         self.bn = nn.BatchNorm2d(beta * in_ch)
         self.bn2 = nn.BatchNorm2d(out_ch)
@@ -119,54 +154,42 @@ class SE_Block(nn.Module):
 
 
 class Refine_Module(nn.Module):
-    def __init__(self):
+    def __init__(self, feature_lv: List[int], feature:int):
         super(Refine_Module, self).__init__()
         self.up_sample1 = nn.Upsample(scale_factor = 2)
         self.up_sample2 = nn.Upsample(scale_factor = 2)
         self.down_sample1 = nn.MaxPool2d(2, 2)
         self.down_sample2 = nn.MaxPool2d(2, 2)
-        self.pw_conv1 = PointWiseConv(768, 512, bs = True)
-        self.pw_conv2 = PointWiseConv(896, 512, bs = True)
-        self.pw_conv3 = PointWiseConv(512, 512, bs = True)
-        self.pw_conv4 = PointWiseConv(512, 256, bs = True)
-        self.pw_conv5 = PointWiseConv(512, 128, bs = True)
-        self.icsp1 = ICSPBlock(512, 512)
-        self.icsp2 = ICSPBlock(512, 512)
+        self.pw_conv1 = PointWiseConv(feature_lv[2]+feature_lv[1], feature, bs = True)
+        self.pw_conv2 = PointWiseConv(feature_lv[2]+feature_lv[0], feature, bs = True)
+        self.pw_conv3 = PointWiseConv(feature, feature, bs = True)
+        self.pw_conv4 = PointWiseConv(feature, feature_lv[1], bs = True)
+        self.pw_conv5 = PointWiseConv(feature, feature_lv[0], bs = True)
+        self.icsp1 = ICSPBlock(feature, feature)
+        self.icsp2 = ICSPBlock(feature, feature)
 
     def forward(self,x: torch.Tensor) -> torch.Tensor:
         x1, x2, x3 = x  # hihg -> low ch
 
-        # x1_up = self.up_sample1(x1)
-        # x = torch.cat([x1_up,x2],dim = 1)
-        #
-        # x = self.pw_conv1(x)
-        # x = self.icsp1(x)
-        # x3_down = self.down_sample1(x3)
-        # x = torch.cat([x,x3_down], dim = 1)
-        # x = self.pw_conv2(x)
-        # x = self.icsp2(x)
         x1_up = self.up_sample1(x1)
-        # x = torch.cat([x1_up,x2],dim = 1)
-        #
-        # x = self.pw_conv1(x)
-        # x = self.icsp1(x)
+        x = torch.cat([x1_up,x2],dim = 1)
+        x = self.pw_conv1(x)
+        x = self.icsp1(x)
         x3_down = self.down_sample1(x3)
-        x = torch.cat([x1_up, x, x3_down], dim = 1)
+        x = torch.cat([x,x3_down], dim = 1)
         x = self.pw_conv2(x)
         x = self.icsp2(x)
-
         ## Refine
         x_up = self.up_sample1(x)
         x_down = self.down_sample2(x)
         x_down = self.pw_conv3(x_down)
         x = self.pw_conv4(x)
         x_up = self.pw_conv5(x_up)
-
         x1 = torch.add(x_down, x1)
         x2 = torch.add(x, x2)
         x3 = torch.add(x_up, x3)
-        return x1, x2, x3
 
+        return x1, x2, x3
 
 class RfFcosHead(nn.Module):
     def __init__(self, head_lv, feature:int, num_class:int):
@@ -182,18 +205,72 @@ class RfFcosHead(nn.Module):
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         reg_sub = self.reg_conv1(x)
         reg_sub = self.reg_conv2(reg_sub)
-
-        cls_sub = self.reg_conv1(x)
-        cls_sub = self.reg_conv2(cls_sub)
+        cls_sub = self.cls_conv1(x)
+        cls_sub = self.cls_conv2(cls_sub)
         cls = self.cls(cls_sub)
         cnt = self.cnt(reg_sub)
         reg = self.reg(reg_sub)
         return cls, cnt, reg
 
+
+class ClassificationSub(nn.Module):
+    def __init__(self, feature, num_class, prior):
+        super(ClassificationSub, self).__init__()
+        self.prior = prior
+        self.cls_c1 = nn.Conv2d(feature, feature, kernel_size = 3, padding = 1)
+        self.cls_c2 = nn.Conv2d(feature, feature, kernel_size = 3, padding = 1)
+        self.cls = nn.Conv2d(feature, num_class, kernel_size = 3, padding = 1)
+        self.bn = nn.BatchNorm2d(feature)
+        self.relu = nn.ReLU(inplace = True)
+        self.apply(init_conv_rand_nomal)
+        nn.init.constant_(self.cls.bias, -np.log((1-self.prior)/self.prior))
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        x = self.relu(self.bn(self.cls_c1(x)))
+        x = self.relu(self.bn(self.cls_c2(x)))
+        cls = self.cls(x)
+        return cls
+
+
+class RegressionSub(nn.Module):
+    def __init__(self,feature):
+        super(RegressionSub, self).__init__()
+        self.reg_c1 = nn.Conv2d(feature, feature, kernel_size = 3, padding = 1)
+        self.reg_c2 = nn.Conv2d(feature, feature, kernel_size = 3, padding = 1)
+        self.reg = nn.Conv2d(feature, 4, kernel_size = 3, padding = 1)
+        self.center = nn.Conv2d(feature, 1, kernel_size=3, padding=1)
+        self.bn = nn.BatchNorm2d(feature)
+        self.relu = nn.ReLU(inplace = True)
+        self.apply(init_conv_rand_nomal)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.relu(self.bn(self.reg_c1(x)))
+        x = self.relu(self.bn(self.reg_c2(x)))
+        reg = self.reg(x)
+        center = self.center(x)
+        return center, reg
+
+
+class ScaleExp(nn.Module):
+    def __init__(self,init_value=1.0):
+        super(ScaleExp,self).__init__()
+        self.scale = nn.Parameter(torch.tensor([init_value],dtype=torch.float32))
+
+    def forward(self,x):
+        return torch.exp(x * self.scale)
+
+
+def init_conv_rand_nomal(module, std: int = 0.01):
+    if isinstance(module, nn.Conv2d):
+        nn.init.normal_(module.weight, std=std)
+
+        if module.bias is not None:
+            nn.init.constant_(module.bias, 0)
+
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = FRFCOS([512, 1024, 2048], 20, 512)
-    print(model)
+    model = FRFCOS([512, 1024, 2048], [128, 256, 512], 20, 256)
+    # print(model)
     # a = torch.Tensor(1, 3, 512, 512)
     # b = model(a)
 
