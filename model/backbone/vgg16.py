d66b02d80f530b493c37356e457e1a6a5e6ff518
clwon1320 <hby1320@gmail.com>
2021-12-14 00:18:07 +0900

    fixed ssd vgg16 ver
    vgg16 setting complete

--- model/backbone/vgg16.py
+++ model/backbone/vgg16.py
@@ -9,19 +9,14 @@ class VGG16(nn.Module):
     def __init__(self):
         super(VGG16, self).__init__()
         vgg16 = torchvision.models.vgg16(pretrained=True)
-        # self.model_node, _ = get_graph_node_names(vgg16)
-
-        self.node = ['features.28', 'features.29', 'features.30']
+        self.model_node, _ = get_graph_node_names(vgg16)
+        print(self.model_node)
+        self.node = ['features.22', 'features.30']
         self.feature_extractor = create_feature_extractor(vgg16, return_nodes=self.node)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        out = self.feature_extractor(x)
-        return out[self.node[0]], out[self.node[1]], out[self.node[2]]
-
-
-
-
-
+        x = self.feature_extractor(x)
+        return x[self.node[0]], x[self.node[1]]
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -29,14 +24,14 @@ if __name__ == '__main__':
     node, _ = get_graph_node_names(model)
     print(node)
     # feature_extractor = create_feature_extractor(model, return_nodes=['features.28', 'features.29', 'features.30'])
-    test = torch.Tensor(1, 3, 512, 512).to(device)
-    out, out1, out2 = model(test)
+    test = torch.Tensor(1, 3, 300, 300).to(device)
+    out, out1 = model(test)
     print(out.size())
     print(out1.size())
-    print(out2.size())
+
 
     # print(out['features.28'].size())
     # print(out['features.29'].size())
     # print(out['features.30'].size())
     # print(f'{out.shape=}')
-    model_info(model, 1, 3, 512, 512, device)
+    model_info(model, 1, 3, 300, 300, device)
