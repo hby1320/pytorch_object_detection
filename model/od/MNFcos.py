1a7f1484bc45c28b8246472c3f6701c671e4d266
clown1320 <hby1320@gmail.com>
2022-04-10 20:47:21 +0900

    [test]MNFCOS
    test MNFCOS structure

--- model/od/MNFcos.py
+++ model/od/MNFcos.py
@@ -5,13 +5,15 @@ from utill.utills import model_info
 from model.backbone.resnet50 import ResNet50v2
 from model.modules.modules import MNBlock, ScaleExp
 from typing import List
+# from model.backbone.efficientnetv1 import EfficientNetV1
 
 
 class MNFCOS(nn.Module):
     def __init__(self, in_channel: List[int], num_class: int, feature: int, freeze_bn: bool = True):
         super(MNFCOS, self).__init__()
         self.backbone = ResNet50v2()
-        self.FeaturePyramidNetwork = LieghtWeightFeaturePyramid(in_channel, feature)
+        # self.backbone = EfficientNetV1(0)
+        self.FeaturePyramidNetwork = LightWeightFeaturePyramid(in_channel, feature)
         self.head = MNHeadFCOS(feature, num_class, 0.01)
         self.backbone_freeze = freeze_bn
 
@@ -33,54 +35,97 @@ class MNFCOS(nn.Module):
         return cls, cnt, reg
 
 
-class LieghtWeightFeaturePyramid(nn.Module):
+class LightWeightFeaturePyramid(nn.Module):
     def __init__(self, in_channel: List[int], feature=256):
-        super(LieghtWeightFeaturePyramid, self).__init__()
-        self.C5DownSample = nn.MaxPool2d(2, 2)
+        super(LightWeightFeaturePyramid, self).__init__()
         self.C5UpSample = nn.Upsample(scale_factor=2)
         self.C5PW1 = nn.Conv2d(in_channels=in_channel[0], out_channels=feature, kernel_size=1)  # BN ACT 고민하
         self.C5PW2 = nn.Conv2d(in_channels=in_channel[0], out_channels=feature, kernel_size=1)
-        self.C4PW = nn.Conv2d(in_channels=in_channel[1], out_channels = feature, kernel_size = 1)
+        self.C4PW = nn.Conv2d(in_channels=in_channel[1], out_channels=feature, kernel_size=1)
         self.C3DownSample = nn.MaxPool2d(2, 2)
-        self.C3PW = nn.Conv2d(in_channels = in_channel[2], out_channels = feature, kernel_size = 1)
-        self.BaseFeature = nn.Conv2d(feature*3, feature, 1, bias=False)
-        self.BaseFeatureBN = nn.BatchNorm2d(feature)
-        self.BaseFeatureACT = nn.SiLU(True)
+        self.C3PW = nn.Conv2d(in_channels=in_channel[2], out_channels=feature, kernel_size=1)
         self.BaseFeatureUpSample = nn.Upsample(scale_factor=2)
         self.BaseFeatureDownSample = nn.MaxPool2d(2, 2)
-        # self.C3MNblock = MNBlock(feature, feature, 3, 3)
-        self.P3MNblock = MNBlock(feature, feature, 3, 3)
-        self.P4MNblock = MNBlock(feature, feature, 3, 3)
-        self.P4MNblockDownSample = nn.MaxPool2d(2, 2)
-        self.P5MNblock = MNBlock(feature, feature, 3, 3)
-        self.P5MNblockDownSample = nn.MaxPool2d(2, 2)
-        self.P6MNblock = MNBlock(feature, feature, 3, 3)
-        self.P6MNblockDownSample = nn.MaxPool2d(2, 2)
-        self.P7MNblock = MNBlock(feature, feature, 3, 3)
+        self.Base = nn.Conv2d(feature * 3, feature, 1, 1, bias=True)
+        self.BaseMNBlock = MNBlock(feature, feature, 3, 3)
+        self.BaseMNBlock2 = MNBlock(feature, feature, 3, 3)
+
+        self.C3MNBlock = MNBlock(feature, feature, 3, 3)
+        self.P3MNBlock = MNBlock(feature, feature, 3, 3)
+        self.P4MNBlock = MNBlock(feature, feature, 3, 3)
+        self.P4MNBlockDownSample = nn.MaxPool2d(2, 2)
+        self.P5MNBlock = MNBlock(feature, feature, 3, 3)
+        self.P5MNBlockDownSample = nn.MaxPool2d(2, 2)
+        self.P6MNBlock = MNBlock(feature, feature, 3, 3)
+
+        self.Base_dw1 = nn.Conv2d(feature*3, feature*3, 5, 1, 5//2, 1, feature*3, bias=False)
+        self.Base_act1 = nn.SiLU(True)
+        self.Base_bn1 = nn.BatchNorm2d(feature*3)
+        self.Base_pw1 = nn.Conv2d(feature*3, feature, 1, 1, bias=True)
+        self.Base_pw2 = nn.Conv2d(feature, feature*3, 1, 1, bias=True)
+        self.Base_dw2 = nn.Conv2d(feature*3, feature*3, 5, 1, 5//2, 1, feature*3, bias=False)
+        self.Base_bn2 = nn.BatchNorm2d(feature * 3)
+        self.Base_act2 = nn.SiLU(True)
+        self.Base_pw3 = nn.Conv2d(feature*3, feature, 1, 1, bias=True)
+        self.Base_pw4 = nn.Conv2d(feature, feature * 3, 1, 1, bias=True)
+        self.Base_dw3 = nn.Conv2d(feature*3, feature*3, 5, 1, 5 // 2, 1, feature*3, bias=False)
+        self.Base_bn3 = nn.BatchNorm2d(feature * 3)
+        self.Base_act3 = nn.SiLU(True)
+        self.Base_pw5 = nn.Conv2d(feature*3, feature, 1, 1, bias=True)
+        self.Dw_3 = nn.Conv2d(feature, feature, 7, 1, 7//2, 1, feature, bias=False)
+        self.Base_bn4 = nn.BatchNorm2d(feature)
+        self.Base_act4 = nn.SiLU(True)
+        self.Base_pw6 = nn.Conv2d(feature * 3, feature, 1, 1, bias=True)
+        self.Base_down = nn.MaxPool2d(2, 2)
+        self.Base_up = nn.Upsample(scale_factor=2)
+        self.DW_1 = nn.Conv2d(feature, feature, 5, 1, 5//2, 1, feature, bias=True)
+        self.DW_2 = nn.Conv2d(feature, feature, 5, 1, 5 // 2, 1, feature, bias=True)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         c3, c4, c5 = x
-        c5_top = self.C5DownSample(c5) # 8
-        c5_top = self.C5PW1(c5_top)
-        c5 = self.C5PW2(c5)  # 32
-        c5 = self.C5UpSample(c5) # 32
-        c4 = self.C4PW(c4) # 32
-        c3 = self.C3PW(c3) #32
-        c3 = self.C3DownSample(c3) # 32
-        # c3_MN = self.C3MNblock(c3)
-        base_feature = torch.concat([c5, c4, c3], dim=1)  # 32
-        base_feature = self.BaseFeature(base_feature)
-        base_feature = self.BaseFeatureBN(base_feature)
-        base_feature = self.BaseFeatureACT(base_feature)
-        base_feature_top = self.BaseFeatureUpSample(base_feature)
+        c5 = self.C5PW1(c5)  # 16
+        c5_2 = self.C5UpSample(c5)  # 32
+        c4 = self.C4PW(c4)  # 32
+        c3 = self.C3PW(c3)  # 64
+        c3_2 = self.C3DownSample(c3)  # 32
 
-        p3 = self.P3MNblock(base_feature_top)  # 64
-        p4 = self.P4MNblock(base_feature)  # 32
-        p5 = self.P4MNblockDownSample(p4)  # 16
-        p5 = self.P5MNblock(p5)
-        p6 = self.P5MNblockDownSample(p5)  # 8
-        # p6 = torch.add(p6, c5_top)
-        p6 = self.P6MNblock(p6)
+        base_feature = torch.concat([c5_2, c4, c3_2], dim=1)  # 256
+        base_1 = self.Base_dw1(base_feature)
+        base_1 = self.Base_bn1(base_1)
+        base_1 = self.Base_act1(base_1)
+        base_1 = self.Base_pw1(base_1)  # pw 384 -> 128
+        base_feature = self.Base_pw6(base_feature)
+
+        base_s = torch.add(base_1, base_feature)
+        base_m = self.Base_pw2(base_s)  # feature*3
+        base_m = self.Base_dw2(base_m)
+        base_m = self.Base_bn2(base_m)
+        base_m = self.Base_act2(base_m)
+        base_m = self.Base_pw3(base_m)
+        base_m = torch.add(base_m, base_s)
+        base_l = self.Base_pw4(base_m)  # feature*3
+        base_l = self.Base_dw3(base_l)
+        base_l = self.Base_bn3(base_l)
+        base_l = self.Base_act3(base_l)
+        base_l = self.Base_pw5(base_l)
+        base_l = torch.add(base_l, base_m)
+        base_l = self.Dw_3(base_l)
+        base_l = self.Base_bn4(base_l)
+        base_l = self.Base_act4(base_l)
+        base_s = self.DW_1(base_s)
+        base_s = self.Base_down(base_s)
+        base_m = self.DW_2(base_m)
+        base_m = self.Base_up(base_m)
+
+        p3 = self.BaseFeatureUpSample(base_l)
+        p3 = torch.add(p3, base_m)
+        p3 = self.P3MNBlock(p3)  # 64
+        p4 = self.P4MNBlock(base_l)  # 32
+        p5 = self.P4MNBlockDownSample(p4)  # 16
+        p5 = torch.add(p5, base_s)
+        p5 = self.P5MNBlock(p5)
+        p6 = self.P5MNBlockDownSample(p5)  # 8
+        p6 = self.P6MNBlock(p6)
         return p3, p4, p5, p6
 
 
@@ -89,20 +134,19 @@ class MNHeadFCOS(nn.Module):
         super(MNHeadFCOS, self).__init__()
         self.class_num = num_class
         self.prior = prior
-        self.block1 = MNBlock(feature, feature, 3, 2)
+        self.block1 = MNBlock(feature, feature, 3, 3)
+
         cls_branch = []
         reg_branch = []
+        cls_branch.append(nn.Conv2d(feature, feature, kernel_size=5, padding=1, groups=feature, bias=False))
+        cls_branch.append(nn.GroupNorm(32, feature))
+        cls_branch.append(nn.SiLU(True))
+        reg_branch.append(nn.Conv2d(feature, feature, kernel_size=5, padding=1, groups=feature, bias=False))
+        reg_branch.append(nn.GroupNorm(32, feature))
+        reg_branch.append(nn.SiLU(True))
 
-        cls_branch.append(nn.Conv2d(feature, feature, kernel_size=3, padding=1, bias=False))
-        cls_branch.append(nn.BatchNorm2d(feature))
-        cls_branch.append(nn.ReLU(True))
-
-        reg_branch.append(nn.Conv2d(feature, feature, kernel_size=3, padding=1, bias=False))
-        reg_branch.append(nn.BatchNorm2d(feature))
-        reg_branch.append(nn.ReLU(True))
-
-        self.cls_conv = nn.Sequential(*cls_branch)
-        self.reg_conv = nn.Sequential(*reg_branch)
+        self.class_conv = nn.Sequential(*cls_branch)
+        self.regression_conv = nn.Sequential(*reg_branch)
 
         self.cls_logits = nn.Conv2d(feature, self.class_num, kernel_size=3, padding=1)
         self.cnt_logits = nn.Conv2d(feature, 1, kernel_size=3, padding=1)
@@ -115,10 +159,11 @@ class MNHeadFCOS(nn.Module):
         cls_logits = []
         cnt_logits = []
         reg_preds = []
-        for index, P in enumerate(inputs):
-            P = self.block1(P)
-            cls_conv_out = self.cls_conv(P)
-            reg_conv_out = self.reg_conv(P)
+
+        for index, feature in enumerate(inputs):
+            feature = self.block1(feature)
+            cls_conv_out = self.class_conv(feature)
+            reg_conv_out = self.regression_conv(feature)
             cls_logits.append(self.cls_logits(cls_conv_out))
             cnt_logits.append(self.cnt_logits(reg_conv_out))
             reg_preds.append(self.scale_exp[index](self.reg_pred(reg_conv_out)))
@@ -127,6 +172,7 @@ class MNHeadFCOS(nn.Module):
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = MNFCOS(in_channel=[2048, 1024, 512], num_class = 20, feature=128).to(device) # Total GFLOPs: 102.2868
+    model = MNFCOS(in_channel=[2048, 1024, 512], num_class=20, feature=128).to(device)
+    # model = MNFCOS(in_channel=[320, 112, 40], num_class = 20, feature=128).to(device) #
     tns = torch.rand(1, 3, 512, 512).to(device)
-    model_info(model, 1, 3, 512, 512, device)  # flop51.26G  para0.03G
+    model_info(model, 1, 3, 512, 512, device, 1)  # flop51.26G  para0.03G
